690b3742b1ddf9a52fd9f106bd3af3e99523f864
mikah <anhminhhoang13@gmail.com>
2018-01-15 05:04:53 -0800

    Updated Result

--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import Cell from './Cell';
 class Result extends React.Component {
-    display = data => {
-        let rowClass = "col-lg-12";
-        let a = data[0];
-        return <Cell rowClass={rowClass} content={a.content} img={a.img}/>
-    }
     render() {
-
         return (<div className="row">
             <div className=" col-lg-12">
                 <div className="result">
